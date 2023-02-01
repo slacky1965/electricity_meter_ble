70b62d63acffd5c3a8025c8c9f0abbaa6a1b9869
Fedor <lfa1@yandex.ru>
2023-02-01 15:25:12 +0300

    added pkt error

--- src/include/device.h
+++ src/include/device.h
@@ -1,6 +1,16 @@
 #ifndef SRC_INCLUDE_DEVICE_H_
 #define SRC_INCLUDE_DEVICE_H_
 
+typedef enum _pkt_err_t {
+    PKT_OK  = 0,
+    PKT_ERR_TIMEOUT,
+    PKT_ERR_UNKNOWN_FORMAT,
+    PKT_ERR_DIFFERENT_COMMAND,
+    PKT_ERR_INCOMPLETE,
+    PKT_ERR_CRC,
+    PKT_ERR_UART
+} pkt_err_t;
+
 #if (ELECTRICITY_TYPE == KASKAD_1_MT)
 #include "kaskad_1_mt.h"
 #elif (ELECTRICITY_TYPE == KASKAD_11)
@@ -8,7 +18,6 @@
 
 extern uint8_t  tariff_changed;
 extern uint8_t  pv_changed;
-
 
 extern void measure_meter();
 
