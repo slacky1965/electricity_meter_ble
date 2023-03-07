6f2165673924b595366d1847ca676f1049440399
Fedor <lfa1@yandex.ru>
2023-03-07 20:28:06 +0300

    Update project.mk

--- make/project.mk
+++ make/project.mk
@@ -5,6 +5,7 @@ $(OUT_PATH)/src/cfg.o \
 $(OUT_PATH)/src/log.o \
 $(OUT_PATH)/src/bthome.o \
 $(OUT_PATH)/src/app_att.o \
+$(OUT_PATH)/src/app_adc.o \
 $(OUT_PATH)/src/cmd_parser.o \
 $(OUT_PATH)/src/ble.o \
 $(OUT_PATH)/src/ccm.o \
@@ -13,6 +14,7 @@ $(OUT_PATH)/src/app_uart.o \
 $(OUT_PATH)/src/device.o \
 $(OUT_PATH)/src/kaskad_1_mt.o \
 $(OUT_PATH)/src/kaskad_11.o \
+$(OUT_PATH)/src/mercury_206.o \
 $(OUT_PATH)/src/main.o
 
 # Each subdirectory must supply rules for building sources it contributes
